e1e188ca89d46e3629ab78367516716143e70d6e
Eumsuna <sarah9522@naver.com>
2020-07-16 01:56:48 +0900

    대한은행

--- 웹디자인/대한은행/js/script.js
+++ 웹디자인/대한은행/js/script.js
@@ -1,6 +1,15 @@
 // 슬라이드
 var slide = document.querySelectorAll(".slide-img");
 var slideWrap = document.querySelector(".slide-img-wrap");
+//탭전환
+var notice = document.querySelector(".notice");
+var gallery = document.querySelector(".gallery");
+var noticeBtn = document.querySelector(".notice-button");
+var galleryBtn = document.querySelector(".gallery-button");
+// 팝업
+var popupClose = document.querySelector(".popup-close");
+var popup = document.querySelector(".popup");
+var popupOn = document.querySelector(".popup-on");
 
 // 슬라이드
 var slideCounter = 0;
@@ -16,3 +25,21 @@ setInterval(function () {
     slideWrap.style.left = slideCounter * -100 + "%";
   }
 }, 3000);
+
+//탭전환
+galleryBtn.addEventListener("click", function () {
+  gallery.className = "gallery on";
+  notice.className = "notice";
+});
+noticeBtn.addEventListener("click", function () {
+  notice.className = "notice on";
+  gallery.className = "gallery";
+});
+
+//팝업
+popupOn.addEventListener("click", function () {
+  popup.className = "popup on";
+});
+popupClose.addEventListener("click", function () {
+  popup.className = "popup";
+});
